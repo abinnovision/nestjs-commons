0c9efb80593452c635f854d540dfbd3012ec4d2f
Andre Loreth <andre@loreth.co>
2025-12-13 10:20:34 +0100

    build: refactor vitest projects

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
+		projects: [
+			"{packages,example}/*/vitest.config.{m,}ts",
+			"{packages,example}/*/test/{integration,e2e}/vitest.config.{m,}ts",
+		],
 		coverage: {
 			provider: "v8",
-			all: true,
 			include: ["packages/*/src/**/*.{ts,tsx}"],
 			reporter: [["lcovonly", { projectRoot: "./" }], "text"],
 		},
